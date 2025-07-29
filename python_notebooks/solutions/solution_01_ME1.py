08ce90f1f46b346c66e7ff362f2d495386fe998b
WandrilleD <wandrille.duchemin@unibas.ch>
2025-07-29 15:02:30 +0200

    adapted notebook1: EDA

--- python_notebooks/solutions/solution_01_ME1.py
+++ python_notebooks/solutions/solution_01_ME1.py
@@ -1,10 +1,10 @@
-fig,ax = plt.subplots( 1,2,figsize=(15,7))
+fig,ax = plt.subplots( 1,2,figsize=(30,15))
 
-sns.heatmap(X_ph_imputed.corr(),ax=ax[0], cmap='jet' , vmin = 0 , vmax = 1)
-ax[0].set_title("half-min imputed data")
+sns.heatmap(X_ph_meanI.corr(),ax=ax[0], cmap='jet' , vmin = 0 , vmax = 1)
+ax[0].set_title(f"mean imputation : score = {get_avg_replicate_corr(X_ph_meanI):.3f}")
 
-sns.heatmap(X_ph_positive.corr(), ax= ax[1], cmap='jet' , vmin = 0 , vmax = 1)
-ax[1].set_title("removed missing data")
+sns.heatmap(X_ph_knnI.corr(), ax= ax[1], cmap='jet' , vmin = 0 , vmax = 1)
+ax[1].set_title(f"KNN imputation : score = {get_avg_replicate_corr(X_ph_knnI):.3f}")
 
 fig.suptitle('sample correlation')
 plt.tight_layout()
