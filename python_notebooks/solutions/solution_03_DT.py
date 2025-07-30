bfe21a1d2553d6f4b505b020e39726182c9280bd
WandrilleD <wandrille.duchemin@unibas.ch>
2025-07-30 16:54:50 +0200

    nb05 trees: added tree cover dataset

--- python_notebooks/solutions/solution_03_DT.py
+++ python_notebooks/solutions/solution_03_DT.py
@@ -1,16 +1,5 @@
 from sklearn.tree import plot_tree
-fig,ax = plt.subplots(figsize=(25,10))
-plot_tree( grid_tree_roc_auc.best_estimator_ , 
-          feature_names=df_cancer.columns[:-1] , 
-          ax=ax , fontsize=12 , filled=True , impurity=False , precision=3)
-ax.set_title('best single decision tree')
+fig,ax = plt.subplots(figsize=(32,4))
 
-
-## We can see that the first node splits on mean_concave_point,
-## then on mean_perimeter and mean_texture
-## we also al see that below the 2nd layer the nodes don't make the decision change for a threshold of 0.05
-## this can still be useful, especially when we have metrics which accounts for multiple thresholds
-## such as ROC AUC
-##
-## but sometimes it can be interesting to "prune" the useless nodes. See:
-## https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html#sphx-glr-auto-examples-tree-plot-cost-complexity-pruning-py
+_ = plot_tree( grid_tree.best_estimator_ , feature_names=X_train.columns , label='root',
+               fontsize=8 , filled=True , impurity=False , precision=1, ax=ax)
